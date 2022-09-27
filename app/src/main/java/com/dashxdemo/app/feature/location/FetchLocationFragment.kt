d73fd73c64afce7a27bd904732d786f7d8bb88a4
sravan9 <sravan@tilicho.in>
2022-09-27 17:04:38 +0530

    Implemented fetch location screen.

---
+++ app/src/main/java/com/dashxdemo/app/feature/location/FetchLocationFragment.kt
@@ -0,0 +1,104 @@
+package com.dashxdemo.app.feature.location
+
+import android.Manifest
+import android.content.Context.LOCATION_SERVICE
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.Fragment
+import com.dashxdemo.app.R
+import com.dashxdemo.app.databinding.FragmentFetchLocationBinding
+import com.dashxdemo.app.utils.Utils.Companion.showToast
+
+
+class FetchLocation : Fragment() {
+
+    private var location: Location? = null
+
+    private val coarseLocationRequestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+        if (isGranted) {
+            location = getLocation()
+            binding.coarseLatitudeTextView.text = location?.latitude.toString()
+            binding.coarseLongitudeTextView.text = location?.longitude.toString()
+        } else {
+            requestPermissions(arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION), 811)
+        }
+    }
+
+    private val fineLocationRequestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+        if (isGranted) {
+            location = getLocation()
+            binding.fineLatitudeTextView.text = location?.latitude.toString()
+            binding.fineLongitudeTextView.text = location?.longitude.toString()
+        } else {
+            requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 812)
+        }
+    }
+
+    private lateinit var binding: FragmentFetchLocationBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        binding = FragmentFetchLocationBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setUpUi()
+    }
+
+    private fun setUpUi() {
+        binding.coarseLocationButton.setOnClickListener {
+            coarseLocationRequestPermissionLauncher.launch(Manifest.permission.ACCESS_COARSE_LOCATION)
+        }
+
+        binding.fineLocationButton.setOnClickListener {
+            fineLocationRequestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            811 -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    location = getLocation()
+                    binding.coarseLatitudeTextView.text = location?.latitude.toString()
+                    binding.coarseLongitudeTextView.text = location?.longitude.toString()
+                } else {
+                    showToast(requireContext(), getString(R.string.location_permission_denied))
+                }
+            }
+
+            812 -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    location = getLocation()
+                    binding.fineLatitudeTextView.text = location?.latitude.toString()
+                    binding.fineLongitudeTextView.text = location?.longitude.toString()
+                } else {
+                    showToast(requireContext(), getString(R.string.location_permission_denied))
+                }
+            }
+        }
+    }
+
+    private fun getLocation(): Location? {
+        val locationManager = requireActivity().getSystemService(LOCATION_SERVICE) as LocationManager?
+        var location: Location? = null
+
+        try {
+            location = locationManager!!.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return location
+    }
+}
